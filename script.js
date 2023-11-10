64f2a35a87527e5c5d51fa41e4de105752ab71c2
augustobrigo <profeaugustobriga@gmail>
2023-11-10 09:23:39 +0100

    crud

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ function cargarTabla(){
  "<div class='col-lg-2 text-center' >ELIMINAR</div>"+
  "<div class='col-lg-2 text-center' >MODIFICAR</div></div>";
  var xhr=new XMLHttpRequest();
- xhr.onreadystatechange=cargar;
+ xhr.onreadystatechange=function(){
+
 
      //CONTROL DE ESTADO DE LA PETICIÓN DE DATOS Y DEL ESTADO DEL SERVIDOR
      if (this.readyState==4 && this.status==200){
@@ -30,34 +31,41 @@ function cargarTabla(){
        objeto.forEach(recorrer);
      
        function recorrer(clientes,index){
+        console.log("vector"+clientes.dni);
         let vector=[clientes.dni,clientes.nombre,clientes.apellido,clientes.telefono];
-         bloqueHtml.innerHTML+="<tr> "+
-         "<td>"+clientes.dni+"</td>"+
-         "<td>"+clientes.nombre+"</td>"+
-         "<td>"+clientes.apellido+"</td>"+
-         "<td>"+clientes.telefono+"</td>"+
+         bloqueHtml.innerHTML+="<div class='row'> "+
+         "<div class='col-lg-2 text-center'"+clientes.dni+"</div>"+
+         "<div class='col-lg-2 text-center'"+clientes.nombre+"</div>"+
+         "<div class='col-lg-2 text-center'"+clientes.apellido+"</div>"+
+         "<div class='col-lg-2 text-center'"+clientes.telefono+"</div>"+
          //simular botón con a href añado clase btn btn-danger (color rojo)
-         "<td><a class='btn btn-danger btn-md'"+
+         "<div class='col-lg-2 text-center'><a class='btn btn-danger btn-md'"+
          //anulo el href, no hay link , pero sí hay evento onclick con 
          //parámetro incluido: dni de esa tupla
          " href='javascript:void(0)' onclick=eliminar(\'"+clientes.dni+"\')>"+
          //texto del botón e icono
          "ELIMINAR<span class='glyphicon glyphicon-trash'></span> </a></td> "+
          //td del modificar
-         "<td><a class='btn btn-info btn-md' "+
+         "<div class='col-lg-2 text-center'><a class='btn btn-info btn-md' "+
          "href='javascript:void(0)' onclick=modificar("+vector+")</td>"+
          "MODIFICAR<span class='glyphicon glyphicon-pencil'></span> </a></td></tr> ";
-          
+       }
        }
      } 
- }
- //PRIMERO HAY QUE HACER LA PETICIÓN
+      //PRIMERO HAY QUE HACER LA PETICIÓN
  xhr.open("GET"," http://moralo.atwebpages.com/menuAjax/clientes/getClientes.php",true);
  xhr.send();
-
- 
+  
  tabla.appendChild(bloqueHtml);
-}
+ }
+ 
+
 function insertarUsuario(){
+
+}
+function eliminar(dni){
+
+}
+function modificar(vector){
 
 }
